372b21e6db5b7df1a00598569997a2236110d218
Рубо Артем Сергеевич <318218@college.local>
2021-03-19 08:13:12 +0300

    FirstCommit

---
+++ Gun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPLab1
+{
+    public class Gun
+    {
+        public string Name { get; set; }
+        public float Caliber { get; set; }
+        public double Range { get; set; }
+        public Gun(string Name,float Caliber,double Range)
+        {
+            this.Name = Name;
+            this.Caliber = Caliber;
+            this.Range = Range;
+        }
+    }
+}
